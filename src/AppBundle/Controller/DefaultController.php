3351481cecd957e41033803c15eb943dd0066e29
Yann Di Serio <diserio.yann@gmail.com>
2016-11-21 08:57:14 +0000

    Add directories for the bundle life.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
+    public function listAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
