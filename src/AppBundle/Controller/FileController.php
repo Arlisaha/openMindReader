56be329154b6f4992edcf2aca83f29d862808c50
Yann Di Serio <diserio.yann@gmail.com>
2017-01-09 10:46:50 +0000

    Add the possibility to read files from directories and display them as a tree list.

--- src/AppBundle/Controller/FileController.php
+++ src/AppBundle/Controller/FileController.php
@@ -2,6 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use \RecursiveDirectoryIterator;
+use \RecursiveIteratorIterator;
+use \RegexIterator;
+use \RecursiveRegexIterator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,18 +14,32 @@ class FileController extends Controller
     public function listAction()
     {
 		$directoryPath = $this->container->getParameter('app.file.folder_path');
+	
+		$directory = new RecursiveDirectoryIterator($directoryPath);
+		$iterator = new RecursiveIteratorIterator($directory);
+		$regex = new RegexIterator($iterator, '/^.+\.mm$/i', RecursiveRegexIterator::GET_MATCH);
+		$fileList = iterator_to_array($regex);
+		array_walk($fileList, function(&$value, $key, $path) {
+			$value = str_replace($path, '', $value[0]);
+		}, $directoryPath);
 		
-		$directoryContent = scandir($directoryPath);
-		
-		$filesList = [];
-		foreach($directoryContent as $file) {
-			if(($file !== '.') && ($file !== '..') && (is_file($directoryPath.$file))) {
-				$filesList[] = $file;
+		$newFileList = [];
+		foreach($fileList as $filePath => $fileName) {
+			if(preg_match('~^(.*)/.*\.mm$~', $fileName, $matches)) {
+				$key = $matches[1];
+				if(!array_key_exists($key, $newFileList)) {
+					$newFileList[$key] = [];
+				}
+				$newFileList[$key][urlencode($fileName)] = str_replace($key.'/', '', $fileName);
 			}
+			else {
+				$newFileList[urlencode($fileName)] = $fileName;
+			}
+			
 		}
-		
+			
         return $this->render('AppBundle::file/list.html.twig', [
-            'file_list' => $filesList,
+            'file_list' => $newFileList,
         ]);
     }
 }
